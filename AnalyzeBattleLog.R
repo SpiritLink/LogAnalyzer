65b9043af36aeb00abd6928a7bb2bae3ae8849d6
유지상 <digndign2@gmail.com>
2021-03-09 00:45:11 +0900

    gsub를 이용해 % 제거

--- AnalyzeBattleLog.R
+++ AnalyzeBattleLog.R
@@ -1,4 +1,7 @@
-line = readLines("sample3.log")
+# install.packages("ggplot2")
+library(ggplot2)
+
+line = readLines("E:/Projects/R/LogAnalyzer/sample.log")
 
 
 times = as.list(NA)
@@ -9,19 +12,23 @@ for(item in line)
 {
   if(grepl('ProcessMonitor', item) == TRUE)
   {
-    
-    # print(item)
     spritresult = strsplit(item, "\\s+")
     separate = unlist(spritresult)
   
     times[length(times) + 1] <- separate[2]
-    mins[length(mins) + 1] <- separate[19]
-    maxs[length(maxs) + 1] <- separate[28]
+    mins[length(mins) + 1] <- as.numeric(gsub("%", "", separate[19]))
+    maxs[length(maxs) + 1] <- as.numeric(gsub("%", "", separate[28]))
   }
 }
 
-for(item2 in times)
-{
-  print(item2)
-}
+times[1] <- NULL
+mins[1] <- NULL
+maxs[1] <- NULL
+myTimes <- data.frame(times)
+myMins <- data.frame(mins)
+myMaxs <- data.frame(maxs)
 
+df <- data.frame(myMins, myMaxs)
+
+str(df)
+# ggplot(df, aes(x = times, y = mins))
