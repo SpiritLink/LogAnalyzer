366430d7db0b7d6a40d59cc9f1552d3defaf0157
유지상 <digndign2@gmail.com>
2021-03-08 23:26:28 +0900

    문자열에서 날짜, 수치 추출

--- AnalyzeBattleLog.R
+++ AnalyzeBattleLog.R
@@ -1,9 +1,27 @@
-line = readLines("sample.log")
+line = readLines("sample3.log")
+
+
+times = as.list(NA)
+mins = as.list(NA)
+maxs = as.list(NA)
 
 for(item in line)
 {
   if(grepl('ProcessMonitor', item) == TRUE)
   {
-    print(item)
+    
+    # print(item)
+    spritresult = strsplit(item, "\\s+")
+    separate = unlist(spritresult)
+  
+    times[length(times) + 1] <- separate[2]
+    mins[length(mins) + 1] <- separate[19]
+    maxs[length(maxs) + 1] <- separate[28]
   }
 }
+
+for(item2 in times)
+{
+  print(item2)
+}
+
